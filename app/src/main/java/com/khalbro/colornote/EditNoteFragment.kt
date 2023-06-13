256ef86d23edcb4b5f492731578f0bec4acb6052
Brav9 <vskh93.009@gmail.com>
2023-06-13 15:43:02 +0300

    Отрисовка UI приложения.

---
+++ app/src/main/java/com/khalbro/colornote/EditNoteFragment.kt
@@ -0,0 +1,23 @@
+package com.khalbro.colornote
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditNoteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+    }
+}
